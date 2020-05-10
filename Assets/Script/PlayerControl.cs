b3e6023c18e244407d76bc241348da433d128a34
Locketgoma <gom@kiwing.co.kr>
2020-05-11 00:08:49 +0900

    PMG 리더기 완성

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 0.5f;
+    public float jumpPower = 10f;
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            gameObject.transform.Translate(new Vector3(1, 0, 0)* speed *Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            gameObject.transform.Translate(new Vector3(-1, 0, 0) * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            gameObject.transform.Translate(new Vector3(0, 0, 1) * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            gameObject.transform.Translate(new Vector3(0, 0, -1) * speed * Time.deltaTime);
+        }
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            gameObject.GetComponentInChildren<Rigidbody>().AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        }
+
+
+    }
+
+
+
+}

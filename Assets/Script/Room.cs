cac22b1af2b019b32ba91cd1f21e94c147bd71b4
Locketgoma <gom@kiwing.co.kr>
2020-05-10 23:20:17 +0900

    폴더 이동, 기본 맵 적용

---
+++ Assets/Script/Room.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    [SerializeField] private int RoomNo;   //방 번호
+    [SerializeField] private int Axis_LX;
+    [SerializeField] private int Axis_LY;
+    [SerializeField] private int Axis_RX;
+    [SerializeField] private int Axis_RY;
+    public RoomManager roomManager;
+
+
+    private void Start() {
+        roomManager = transform.parent.gameObject.GetComponent<RoomManager>();
+    }
+
+    public void Initialized(RoomData roomInput) {
+        RoomNo = roomInput.RoomNo;
+        Axis_LX = roomInput.Axis_LX;
+        Axis_LY = roomInput.Axis_LY;
+        Axis_RX = roomInput.Axis_RX;
+        Axis_RY = roomInput.Axis_RY;
+
+        Debug.Log(RoomNo);
+
+        InitBatch();
+    }
+
+    public void InitBatch() {
+        gameObject.transform.position = new Vector3(Axis_LX, 0,Axis_LY );
+        gameObject.transform.localScale = new Vector3(Axis_RX - Axis_LX, 1,Axis_RY - Axis_LY);
+
+    }
+
+
+}
---
+++ Assets/Script/RoomManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    [SerializeField] private LoadData loadData;
+    [SerializeField] private GameObject[] RoomList;     //Room List
+    [SerializeField] private GameObject RoomSample;     //Room Sample Prefab
+    [SerializeField] private int roomCount;
+    public JsonParser jsonParser;
+    public int RoomCount { get { return roomCount; } set { roomCount = value; } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jsonParser.Init();
+        loadData = jsonParser.LoadData;
+        roomCount = loadData.RoomCount;
+        Debug.Log(loadData.RoomCount);
+        RoomList = new GameObject[loadData.RoomCount];
+        
+
+        for (int i = 0; i < roomCount; i++) {
+            Debug.Log(loadData.Room[i].RoomNo);
+            Instantiate(RoomSample).transform.parent = gameObject.transform; ;
+            RoomList[i] = transform.GetChild(i).gameObject;
+            RoomList[i].GetComponent<Room>().Initialized(loadData.Room[i]);
+        }
+    }
+
+
+}
